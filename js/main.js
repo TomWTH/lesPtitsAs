1c4fc6593e4a461a603f1af3f9f335558da1bcf2
Thomas Troussicot <ttroussicot@fastbooking.net>
2017-08-03 15:24:07 +0200

    feat(global site): add loader + sticky Nav + button style

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
-console.log('pop');
+$(function() {
+  'use strict';
+  var lesPtitsAs = {
 
-$(window).on('load', function() {
-  event.preventDefault();
-  $('.plop').addClass('nope');
+    // Call or not my methods
+    init: function() {
+      // loading logo
+      this.loadingLogo();
+      this.stickyNav();
+
+    },
+
+    // Methods
+    loadingLogo: function() {
+      $(window).on('load', function() {
+        $('.plop').addClass('nope');
+      });
+    },
+
+    stickyNav : function() {
+      var headerMinusNav = ($('.header').height())-($('.nav-desktop').height());
+      $(window).scroll(function() {
+        if ($(this).scrollTop()>=headerMinusNav) {
+          $('.nav-desktop').addClass('sticky-nav');
+        }
+        else {
+          $('.nav-desktop').removeClass('sticky-nav');
+        }
+      });
+    },
+  }
+  // Call instance of my object
+  lesPtitsAs.init();
 });
